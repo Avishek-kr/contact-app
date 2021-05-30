9b9e35066df3466e49bcf64adb3c8637bf6d8827
Avishek-kr <ak251997@gmail.com>
2021-05-30 18:27:27 +0530

    delete contact

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
 class App extends Component {
+    state={
+      contacts:[
+        {
+          "id": "ryan",
+          "name": "Ryan Florence",
+          "email": "ryan@reacttraining.com",
+          "avatarURL": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        },
+        {
+          "id": "michael",
+          "name": "Michael Jackson",
+          "email": "michael@reacttraining.com",
+          "avatarURL": "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "email": "tyler@reacttraining.com",
+          "avatarURL": "https://i.pinimg.com/originals/73/16/f5/7316f550de9ca0045e3d8d98a5bb5e44.png"
+        }
+      ]
+    }
+removeContact =(contact)=>{
+  this.setState((state)=>({
+    contacts: state.contacts.filter((c)=>c.id !== contact.id)
+  }))
+}
 
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     )
   }
